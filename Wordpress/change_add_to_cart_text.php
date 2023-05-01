931c72651787eea4da676f7cc2c55158e2baa549
ravi.kumar@willshall.com <ravi.kumar@willshall.com>
2023-05-01 17:57:07 +0530

    change add to caryt text

---
+++ Wordpress/change_add_to_cart_text.php
@@ -0,0 +1,10 @@
+//change add to cart text	
+	function custom_add_to_cart_text( $text, $product ) {
+		if ( $product->is_type( 'simple' ) ) {
+			$text = __( 'Buy Now', 'woocommerce' );
+		} elseif ( $product->is_type( 'variable' ) ) {
+			$text = __( 'Select Options', 'woocommerce' );
+		}
+		return $text;
+	}
+	add_filter( 'woocommerce_product_add_to_cart_text', 'custom_add_to_cart_text', 10, 2 );
